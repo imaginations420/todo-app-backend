a6382345c1194834b6e156935ee86d93f4ed1b8b
sreekanthd <155742570+imaginations420@users.noreply.github.com>
2024-10-03 12:41:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const todoRoutes = require('./todos');
-const authRoutes = require('./auth'); // Assuming auth routes are in a separate file
+const authRoutes = require('./auth');
 require('dotenv').config();
 
 const app = express();
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
-app.use('/api', authRoutes); // Authentication routes
-app.use('/api', todoRoutes); // To-Do routes
+app.use('/api', authRoutes);
+app.use('/api', todoRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
