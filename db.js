b718f169bdc72701f31246a64c868e13143c805b
sreekanthd <155742570+imaginations420@users.noreply.github.com>
2024-10-03 12:39:48 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-// Change the path to your database file
+
 const db = new sqlite3.Database(path.join(__dirname, 'my-database.db'));
 
-// Create tables if they don't exist
+
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
